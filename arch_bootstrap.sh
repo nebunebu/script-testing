b1dd17ff6d2a6b593df52ed0ce20f61ab2a65a7f
nebu <nebu.nebuchadnezzar@gmail.com>
2022-12-25 23:33:15 -0500

    typo

--- arch_bootstrap.sh
+++ arch_bootstrap.sh
@@ -27,4 +27,4 @@ gzip -d '/usr/share/kbd/keymaps/i386/qwerty/us_swap_esc_caps.map.gz'
 sed -i -e 's/1 = Escape/58 = Escape/' -e 's/58 = Caps_Lock/1 = Caps_Lock/' \
   '/usr/share/kbd/keymaps/i386/qwerty/us_swap_esc_caps.map'
 gzip '/usr/share/kbd/keymaps/i386/qwerty/us_swap_esc_caps.map'
-loadkeys us_swap_caps_esc
+loadkeys us_swap_esc_caps
