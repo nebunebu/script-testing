3bdcc9ebb403a9365b3a796d4535e726f798d19c
nebu <nebu.nebuchadnezzar@gmail.com>
2022-12-25 23:37:21 -0500

    minor edit

--- arch_bootstrap.sh
+++ arch_bootstrap.sh
@@ -18,12 +18,12 @@
 ###################################
 # Set the console keyboard layout #
 # #################################
-# swaps ESC with Caps_Lock
+
+# Sets keymap to us qwerty with esc and caps_lock keys swapped
 
 cp /usr/share/kbd/keymaps/i386/qwerty/us.map.gz \
   /usr/share/kbd/keymaps/i386/qwerty/us_swap_esc_caps.map.gz 
 gzip -d '/usr/share/kbd/keymaps/i386/qwerty/us_swap_esc_caps.map.gz'
-# Swap Esc and CapsLock
 sed -i -e 's/1 = Escape/58 = Escape/' -e 's/58 = Caps_Lock/1 = Caps_Lock/' \
   '/usr/share/kbd/keymaps/i386/qwerty/us_swap_esc_caps.map'
 gzip '/usr/share/kbd/keymaps/i386/qwerty/us_swap_esc_caps.map'
